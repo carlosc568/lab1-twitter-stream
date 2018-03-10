75c7124f898c3466e8c1833af4c2f8c6f2935902
Carlos Sánchez <680321@unizar.es>
2018-03-10 14:40:29 +0100

    first commit

---
+++ src/main/java/es/unizar/tmdad/lab1/controller/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package es.unizar.tmdad.lab1.controller;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/twitter").withSockJS();
+    }
+    
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/queue/");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+}
---
+++ src/main/java/es/unizar/tmdad/lab1/service/SimpleStreamListener.java
@@ -0,0 +1,39 @@
+package es.unizar.tmdad.lab1.service;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.social.twitter.api.Tweet;
+import org.springframework.util.MimeTypeUtils;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.social.twitter.api.StreamDeleteEvent;
+import org.springframework.social.twitter.api.StreamWarningEvent;
+
+import org.springframework.social.twitter.api.StreamListener;
+
+public class SimpleStreamListener implements StreamListener {
+	
+   private SimpMessageSendingOperations ops;
+   private String query;
+
+   public SimpleStreamListener(SimpMessageSendingOperations  ops, String query){
+       this.ops = ops;
+       this.query = query;
+   }
+
+   
+   @Override
+   public void onTweet(Tweet tweet) {
+	   //System.exit(0);
+       Map<String,Object> map = new HashMap<>();
+       map.put(MessageHeaders.CONTENT_TYPE, MimeTypeUtils.APPLICATION_JSON);
+       ops.convertAndSend("/queue/search/" + query, tweet, map);
+   }
+   
+   	@Override
+   	public void onWarning(StreamWarningEvent e) {}
+    @Override
+    public void onDelete(StreamDeleteEvent e) {}
+    @Override
+    public void onLimit(int n) {}
+}
